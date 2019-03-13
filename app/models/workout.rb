734fd9054b344210985aee290f8c4784d39d20ec
inkanimal <theatomproject@gmail.com>
2019-03-13 14:27:50 -0400

    Create workout.rb

---
+++ app/models/workout.rb
@@ -0,0 +1,23 @@
+class Workout < ApplicationRecord
+  belongs_to :user
+  has_many :worksets, inverse_of: :workout
+  has_many :exercises, through: :worksets
+  accepts_nested_attributes_for :worksets, allow_destroy: true
+
+  validates :name, presence: true
+
+  def exercise_num(num)
+    num.to_i.times { self.worksets.build.build_exercise } if num
+  end
+
+  def delete_workout
+    self.exercises.each(&:destroy)
+    self.worksets.destroy_all
+    self.destroy
+  end
+
+  def delete_exercises
+    self.exercises.each(&:destroy)
+    self.worksets.destroy_all
+  end
+end
