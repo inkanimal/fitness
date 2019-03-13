d4a26913eab671418e75fbd96539e304ec1140ce
inkanimal <theatomproject@gmail.com>
2019-03-13 14:26:30 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users, controllers: {registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :workouts
+  resources :exercises
+  resources :worksets
 
 
 
